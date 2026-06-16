dea7d902c0d5f1b0735d77d44a32a38d26bb0822
Theo Wang <th.trabajos.wang@gmail.com>
2026-06-16 01:20:59 +0000

    Implementa selection sort

--- algoritmos.c
+++ algoritmos.c
@@ -17,11 +17,32 @@ void bubble_sort(int *arr, int n) {
 }
 
 void selection_sort(int *arr, int n) {
-    /* TODO: implementar */
-    (void)arr;
-    (void)n;
-}
+    //se crean variables que guardan posicion y una temporal para el intercambio de variables
+    int min_idx;
+    int variable_temp; 
 
+    //se realiza un primer ciclo hasta u numero menor del arreglo, pues el ultimo valor se acomoda por si solo
+    for (int i = 0; i < n - 1; i++){
+
+        //se guarda la posicion inicial en una variable i para luego ser utillizada en recorrer el puntero
+        min_idx = i; 
+
+        // j empieza una posicion mas a la de i, pues lo que se busca es compararlo con los valores siguientes
+        for (int j = i +1; j < n; j++){
+            
+            //si el valor del puntero en j es menor que el valor del puntero en la inicial se intercambian POSICIONES
+            if (*(arr + j) < *( arr + min_idx)){
+
+                //esto permite que luego se compare sobre el menor del total de una lista desordenada
+                min_idx = j;
+            }
+        }
+        //por utimo se guarda el valor de la posicion inicialtemporalmente y luego se intercambian posiciones
+        variable_temp = *(arr + i); 
+        *(arr + i) = *(arr + min_idx);
+        *(arr + min_idx) = variable_temp;
+     }
+}
 void insertion_sort(int *arr, int n) {
     /* TODO: implementar */
     (void)arr;
